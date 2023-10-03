45038f886940924b06abba9c17511d5ed69d98e4
mehdinoori20 <mehdi.noori1994@hotmail.com>
2023-10-03 21:18:27 +0200

    examples

--- src/com/Mehdi/demo/Main.java
+++ src/com/Mehdi/demo/Main.java
@@ -11,6 +11,14 @@ public class Main {
             for (int i = 1; i <= n; i++) {
                 System.out.println("Värde: " +i);
             }
+            // if-else check if the nr is bigger than 5
+            int num = 17;
+            if (num > 7) {
+                System.out.println("Num är större än 7");
+            } else {
+                System.out.println("Num är inte större än 7");
+
+            }
         }
     }
 

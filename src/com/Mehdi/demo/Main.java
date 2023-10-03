4442b999a726ea6a79b177886ac62dfc69131ed4
mehdinoori20 <mehdi.noori1994@hotmail.com>
2023-10-03 21:03:45 +0200

    First Commit

---
+++ src/com/Mehdi/demo/Main.java
@@ -0,0 +1,16 @@
+package com.Mehdi.demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    skrivUtNummer(10);
+
+}
+        public static void skrivUtNummer(int n) {
+            for (int i = 1; i <= n; i++) {
+                System.out.println("Värde: " +i);
+            }
+        }
+    }
+
